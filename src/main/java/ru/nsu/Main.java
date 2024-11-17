ce751678c6d53b2d293842493791c6f872c3701b
anastasiya <nastya050620031n@gmail.com>
2024-11-18 00:28:32 +0700

    added entering lines

--- src/main/java/ru/nsu/Main.java
+++ src/main/java/ru/nsu/Main.java
@@ -1,9 +1,42 @@
 package ru.nsu;
 
 import java.io.IOException;
+import java.util.Scanner;
+
+import static ru.nsu.LSBReplacementAlgorithm.embedMessage;
+import static ru.nsu.LSBReplacementAlgorithm.extractMessage;
 
 public class Main {
+
+    private static final String INPUT_JPG_PATH = "./forest.png";
+    private static final String OUTPUT_JPG_PATH = "./embedded_forest.png";
+
     public static void main(String[] args) throws IOException {
         Test.test();
+        embedEnteredLines();
+    }
+
+    public static void embedEnteredLines() throws IOException {
+        System.out.println("-------------------------");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("Введите строку (для выхода введите 'exit'):");
+
+        while (true) {
+            System.out.print("> ");
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Выход из программы.");
+                break;
+            }
+
+            System.out.println("Сообщение, которое будет внедряться: " + input);
+            embedMessage(INPUT_JPG_PATH, OUTPUT_JPG_PATH, input);
+            String extractedMessage = extractMessage(OUTPUT_JPG_PATH);
+            System.out.println("Извлеченное сообщение: " + extractedMessage);
+        }
     }
 }
--- src/main/java/ru/nsu/Test.java
+++ src/main/java/ru/nsu/Test.java
@@ -14,16 +14,16 @@ public class Test {
         String message = "Лабораторная работа 6. Элементы стеганографии.";
         System.out.println("Сообщение, которое будет внедряться: " + message);
         embedMessage(INPUT_JPG_PATH, OUTPUT_JPG_PATH, message);
-
         String extractedMessage = extractMessage(OUTPUT_JPG_PATH);
         System.out.println("Извлеченное сообщение: " + extractedMessage);
+
+        System.out.println("------------------------");
 
         message = "Любимым занятием нас всех, мальчишек, было лазание на вековые сосны. Мы забирались на" +
                 " самые вершины. Оттуда, казалось, можно было дотянуться рукой до пышных летних облаков. Там сильно," +
                 " до одури пахло нагретой смолой и во все стороны простирался великий неведомый лес.";
         System.out.println("Сообщение, которое будет внедряться: " + message);
         embedMessage(INPUT_JPG_PATH, OUTPUT_JPG_PATH, message);
-
         extractedMessage = extractMessage(OUTPUT_JPG_PATH);
         System.out.println("Извлеченное сообщение: " + extractedMessage);
     }
